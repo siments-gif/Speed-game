3d328c88d05a65671cd13ffbad0855ef28c3b77a
Simen Skogen <sim1skogen@gmail.com>
2024-09-13 10:20:33 +0200

    Started cleaning up app by handle introScreen inside a different component so app.jsx is more redable.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import GameBoard from './components/gameBoard/gameBoard'
 
 function App() {
-  const [showBoard, setShowBoard] = useState(false)
-  
-  const endGame = () => {
-    setShowBoard(false)
-  }
   return (
     <>
-    {showBoard ? (
-      <GameBoard />
-    ) : (
-    <section className='introBoard'>
-    <div className='introContent'>
-      <h1>Skriv navnet ditt for å starte spillet</h1>
-      <input type="text" />
-      <button onClick={() => setShowBoard(true)}>Start spill</button>
-    </div>
-    </section>
-  )}  
     </>
   )
 }
---
+++ src/components/introScreen/IntroScreen.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const IntroScreen = ({ onStartGame }) => {
+  const [playerName, setPlayerName] = useState("");
+  
+  const handleGameStart = () => {
+    if(playerName.trim()) {
+        onStartGame(playerName);
+    }
+  };
+  
+  return (
+    <section className='introBoard'>
+    <div className='introContent'>
+      <h1>Skriv navnet ditt for å starte spillet</h1>
+      <input 
+        type="text"
+        placeholder='Simen Skogen'
+        value={playerName}
+        onChange={(e) => setPlayerName(e.target.value)}
+      />
+      <button onClick={handleGameStart}>Start spill</button>
+    </div>
+    </section>
+  )
+}
+
+export default IntroScreen
